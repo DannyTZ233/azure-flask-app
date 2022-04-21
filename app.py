450d9b1c00c14970f8adec048135d06db099eb72
DannyTZ233 <70179486+DannyTZ233@users.noreply.github.com>
2022-04-21 03:54:51 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+
+from flask import Flask
+from flask_restful import Api
+from flask_cors import CORS 
+
+# employee
+
+
+
+app = Flask(__name__)
+
+# CORS(app)
+api = Api(app)
+
+@app.route('/', methods=['GET'])
+def home():
+    return 'test'
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
